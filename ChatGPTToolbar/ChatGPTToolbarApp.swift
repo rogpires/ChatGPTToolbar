ddbd12dfa236c5a7ce568b86d3fa4fa9c65a0033
rogpires <rogeriocpires@hotmail.com>
2023-03-20 22:07:02 -0300

    Initial Commit

---
+++ ChatGPTToolbar/ChatGPTToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTToolbarApp.swift
+//  ChatGPTToolbar
+//
+//  Created by Rogerio Pires on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
